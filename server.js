e866739d9c2928272ed6c874b743fc9d194d2f02
Egmal <45897834+Egmal@users.noreply.github.com>
2018-12-15 16:19:59 +0200

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get("/", function (req, res) {
 // your first API endpoint... 
 app.get("/api/whoami", function (req, res) {
   const data = {
-    'ip': req.headers['x-forwarded-for'], 
+    'ipaddress': req.headers['x-forwarded-for'], 
     'language': req.headers['accept-language'],
     'software': req.headers['user-agent'],
   }
